7601476c5e602589a7e5cda4d6cbe2078e8544ce
Charlot <7wangsong@gmail.com>
2016-01-14 15:22:06 +0800

    test

---
+++ programme/wms/windows/ScmClient/TestConsole/Com.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO.Ports;
+
+namespace TestConsole
+{
+   public class Com
+    {
+
+      public static SerialPort sp;
+       public static void Open() {
+           try
+           { 
+               sp = new SerialPort("COM13");
+               sp.BaudRate = 38400;
+               sp.Open();
+               sp.DataReceived += new SerialDataReceivedEventHandler(sp_DataReceived);
+               Console.WriteLine("opened!");
+              // sp.Close();
+           }
+           catch (Exception e) {
+               Console.WriteLine(e.Message);
+           }
+       }
+
+       static void sp_DataReceived(object sender, SerialDataReceivedEventArgs e)
+       {
+
+           Byte[] recvbuf = new Byte[17];
+           sp.Read(recvbuf, 0, recvbuf.Length);
+
+           string data = System.Text.Encoding.Default.GetString(recvbuf);
+
+           Console.WriteLine(data);
+           //throw new NotImplementedException();
+       }
+    }
+}
--- programme/wms/windows/ScmClient/TestConsole/Program.cs
+++ programme/wms/windows/ScmClient/TestConsole/Program.cs
@@ -10,37 +10,37 @@ namespace TestConsole
     {
         static void Main(string[] args)
         {
-            string msg = "00 FF 11 16 30 98 49 19 02 35 04 90 E3 7F 02 92 FF ".Replace(" ", "");
-            Regex msgRegex = new Regex(@"^(00FF)[A-Z0-9]{28}(FF)$");
-            Match m = msgRegex.Match(msg);
-            Console.WriteLine(m.Success);
+            //string msg = "00 FF 11 16 30 98 49 19 02 35 04 90 E3 7F 02 92 FF ".Replace(" ", "");
+            //Regex msgRegex = new Regex(@"^(00FF)[A-Z0-9]{28}(FF)$");
+            //Match m = msgRegex.Match(msg);
+            //Console.WriteLine(m.Success);
 
-            //Regex carRegex = new Regex(@"[A-E](\w{3})");
-            //Regex boxRegex = new Regex(@"[F-Z0-9](\w{3})");
-            //Match match = carRegex.Match("C1 11 30 98 49 19 00 21 10 50 AC 3D");
-            byte[] recvbuf = new byte[] { 0, 255, 0, 32, 48, 152, 73, 25, 0, 33, 18, 16, 154, 215, 1, 2, 255 };
-            string data = string.Empty;
-            foreach (byte b in recvbuf)
-            {
-                //string bb = b.ToString("X0");
-                //if (bb.Length == 1) {
-                //    bb = "0" + bb;
-                //}
-                //data +=bb + " ";
-                data += b.ToString("X0").PadLeft(2, '0');
-            }
+            ////Regex carRegex = new Regex(@"[A-E](\w{3})");
+            ////Regex boxRegex = new Regex(@"[F-Z0-9](\w{3})");
+            ////Match match = carRegex.Match("C1 11 30 98 49 19 00 21 10 50 AC 3D");
+            //byte[] recvbuf = new byte[] { 0, 255, 0, 32, 48, 152, 73, 25, 0, 33, 18, 16, 154, 215, 1, 2, 255 };
+            //string data = string.Empty;
+            //foreach (byte b in recvbuf)
+            //{
+            //    //string bb = b.ToString("X0");
+            //    //if (bb.Length == 1) {
+            //    //    bb = "0" + bb;
+            //    //}
+            //    //data +=bb + " ";
+            //    data += b.ToString("X0").PadLeft(2, '0');
+            //}
 
-            Console.WriteLine(data);
-
-
-            string dd = new String(data.Skip(6).Take(36).ToArray()).Replace(" ", "");
-            Console.WriteLine(dd);
+            //Console.WriteLine(data);
 
 
-            string ddd= new String(msg.Skip(4).Take(4).ToArray()).Replace(" ", "");
-            Console.WriteLine(ddd);
+            //string dd = new String(data.Skip(6).Take(36).ToArray()).Replace(" ", "");
+            //Console.WriteLine(dd);
 
 
+            //string ddd= new String(msg.Skip(4).Take(4).ToArray()).Replace(" ", "");
+            //Console.WriteLine(ddd);
+
+            Com.Open();
             Console.Read();
 
         }
