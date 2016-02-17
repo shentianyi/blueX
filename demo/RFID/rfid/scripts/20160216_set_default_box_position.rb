6251a91a3e38600e6e6e77471a710a04b1bda70e
liqites <leechee89@gmail.com>
2016-02-17 08:58:14 +0800

    change

--- demo/RFID/rfid/scripts/20160216_set_default_box_position.rb
+++ demo/RFID/rfid/scripts/20160216_set_default_box_position.rb
@@ -1,7 +1,9 @@
 Position.transaction do
   Warehouse.all.each do |w|
+  unless p=w.positions.where(nr:w.nr).first
     p= w.positions.build(nr: w.nr, name: w.name)
     p.save
+end
     w.order_boxes.update_all(position_id: p.id)
   end
 end
