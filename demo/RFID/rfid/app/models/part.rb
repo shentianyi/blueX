00a509dd404115c9e35b5006fc8f3c9f91de44dd
lzding <472339105@qq.com>
2016-03-01 17:37:01 +0800

    fix movement export

--- demo/RFID/rfid/app/models/part.rb
+++ demo/RFID/rfid/app/models/part.rb
@@ -1,6 +1,7 @@
 class Part < ActiveRecord::Base
   belongs_to :part_type
   belongs_to :color
+  has_many :part_positions, :dependent => :destroy
 
   validates_presence_of :nr, :message => "零件号不能为空!"
   validates_uniqueness_of :nr, :message => "零件号不能重复!"
